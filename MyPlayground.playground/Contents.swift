c6089065efd7b5c7d43e2b6baf2d7101c2aeed20
francis <francis@franciss-MacBook-Pro.local>
2019-02-15 22:30:55 +0800

    improve calendar view

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,49 @@
 import Cocoa
 import CoreFoundation
-let n:Int = 2
-let y = 2019
-let s = String(format: "%04d%02d",y,n)
+class YearMonth{
+    var year:Int
+    var month:Int
+    init() {
+        year = 2019
+        month = 2
+    }
+    var date:Date{
+        get{
+            let calendar = Calendar.current
+            var dateComponent = DateComponents()
+            dateComponent.month = month
+            dateComponent.year = year
+            return calendar.date(from: dateComponent)!
+        }
+    }
+    func toDate()->Date{
+        let calendar = Calendar.current
+        var dateComponent = DateComponents()
+        dateComponent.month = month
+        dateComponent.year = year
+        return calendar.date(from: dateComponent)!
+    }
+    func toDate(Year:Int,Month:Int)->Date{
+        let calendar = Calendar.current
+        var dateComponent = DateComponents()
+        dateComponent.month = Month
+        dateComponent.year = Year
+        return calendar.date(from: dateComponent)!
+    }
+    func preMonth()->Date{
+        let nY = month==1 ? year-1:year
+        let nM = month==1 ? 12:month-1
+        return toDate(Year: nY, Month: nM)
+    }
+    func nextMonth()->Date{
+        let nY = month==12 ? year+1:year
+        let nM = month==12 ? 1:month+1
+        return toDate(Year: nY, Month: nM)
+    }
+}
+let a = YearMonth()
+let b = a.preMonth()
+b
+a.date
+
+
--- Workout/WorkoutCalendarView.swift
+++ Workout/WorkoutCalendarView.swift
@@ -14,37 +14,19 @@ class workoutCalendarViewController: UIViewController,UITableViewDelegate,UITabl
     //组件
     @IBOutlet weak var workoutTableView: UITableView!
     @IBOutlet weak var workoutCalendarView: UICollectionView!
-    @IBAction func preMonth(_ sender: Any) {
-        preMonth()
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    @IBAction func back(_ sender: Any) {
     }
-    @IBAction func nextMonth(_ sender: Any) {
-        nextMonth()
+    @IBAction func editHistory(_ sender: Any) {
+        monthSet.append(monthSet.last!.nextMonth())
+        workoutCalendarView.reloadData()
     }
-    @IBOutlet weak var monthLabel: UILabel!
-    @IBOutlet weak var back: UIBarButtonItem!
-    @IBOutlet weak var editHistory: UIBarButtonItem!
     //数据
-    let today = Date.init()
-    var year:Int = 2019
-    var monthCursor:Date = Date.init(){
-        didSet{
-            workoutCalendarView.reloadData()
-        }
-    }
-    var month :Int = 02{
-        didSet{
-            monthLabel.text = "\(year)年\(month)月"
-            let dateDF = DateFormatter.init()
-            dateDF.dateFormat = "yyyyMM"
-            let dateStr = String(format: "%04d%02d", year,month)
-            monthCursor = dateDF.date(from: dateStr)!
-        }
-    }
+    let calendar = Calendar.current
+    var monthSet:[YearMonth]  = [YearMonth(),YearMonth().nextMonth()]
+    //@deprecated
     var workoutData = [DailyWorkout]()
     var workoutDay = DailyWorkout.init(date: Date.init(), workouts: nil)
-    var calendar = Calendar.current
-    //按钮动作
-    
     //
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,26 +34,50 @@ class workoutCalendarViewController: UIViewController,UITableViewDelegate,UITabl
         //workoutTableView.dataSource = self
         workoutCalendarView.dataSource = self
         workoutCalendarView.delegate = self
-        //更正init错误
-        month = 2
+        flowLayout.sectionHeadersPinToVisibleBounds = true
         //
-        
     }
     //日历CollectionView
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "sectionHeader", for: indexPath) as! workoutCalendarViewSectionHeader
+        let date = monthSet[indexPath.section]
+        sectionHeader.sectionhHeader.text = "\(date.year)年\(date.month)月"
+        return sectionHeader as UICollectionReusableView
+    }
+    //预加载@considering
+    /*
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        //加载下一个月
+        if(indexPath.section>monthSet.count-3){addMonthSection();print(monthSet.count);workoutCalendarView.reloadData()}
+    }
+    */
+    //
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return monthSet.count
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return daysInMonth(day: monthCursor)
+        return 42
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let date = monthSet[indexPath.section]
         let collectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "workoutCalendarViewCell", for: indexPath) as! workoutCalendarViewCell
-        let cellDate = dayInMonth(day: monthCursor, index: indexPath)
+        let cellDate = dayInMonth(day: date.date, index: indexPath)
         if(cellDate == 0){
-            collectionCell.dateBackground.isHighlighted = false
+            collectionCell.isUserInteractionEnabled = false
             collectionCell.date.text = nil
         }else{
-            collectionCell.dateBackground.isHighlighted = false
+            collectionCell.isUserInteractionEnabled = true
             collectionCell.date.text = String(cellDate)
         }
         return collectionCell as UICollectionViewCell
+    }
+    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    //增加月份
+    func addMonthSection(){
+        let endMonth = monthSet.last!
+        monthSet.append(endMonth.nextMonth())
     }
     //每天的锻炼 TableViewDateSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -96,27 +102,19 @@ class workoutCalendarViewController: UIViewController,UITableViewDelegate,UITabl
         get{ return UIStatusBarStyle.lightContent }
     }
     //日历计算
-    func daysInMonth(day:Date)->Int{
-        //let calendar = Calendar.current
-        //let days = calendar.range(of: Calendar.Component.day, in: Calendar.Component.month, for: day)!.endIndex-1
-        return 42
+    func monthDescription(firstDate:Date)->(firstWeekDay:Int,totalDays:Int){
+        let firstWeekDay = calendar.ordinality(of: Calendar.Component.weekday, in: Calendar.Component.weekOfMonth, for: firstDate)!
+        let totalDays = calendar.range(of: Calendar.Component.day, in: Calendar.Component.month, for: firstDate)!.endIndex-1
+        return (firstWeekDay,totalDays)
     }
     func dayInMonth(day:Date,index:IndexPath)->Int{
-        let calendar = Calendar.current
-        let firstWeekDay = calendar.ordinality(of: Calendar.Component.weekday, in: Calendar.Component.weekOfMonth, for: day)!
-        print(firstWeekDay)
-        let totalDays = calendar.range(of: Calendar.Component.day, in: Calendar.Component.month, for: day)!.endIndex-1
-        if(index.item<=totalDays+firstWeekDay-1 && index.item>=firstWeekDay-1){return index.item-firstWeekDay+2}else{return 0}
+        let (firstWeekDay,totalDays) = monthDescription(firstDate: day)
+        if(index.item<=totalDays+firstWeekDay-2 && index.item>=firstWeekDay-1){return index.item-firstWeekDay+2}else{return 0}
     }
-    func nextMonth(){
-        month = month>11 ? 1:month+1
-        year = month>11 ? year+1:year
-    }
-    func preMonth(){
-        month = month==1 ? 12:month-1
-        year = month==1 ? year-1:year
-    }
-    //
+}
+
+class workoutCalendarViewSectionHeader:UICollectionReusableView{
+    @IBOutlet weak var sectionhHeader: UILabel!
 }
 
 class workoutTableViewCell:UITableViewCell{
@@ -129,4 +127,50 @@ class workoutTableViewCell:UITableViewCell{
 class workoutCalendarViewCell:UICollectionViewCell{
     @IBOutlet weak var dateBackground: UIImageView!
     @IBOutlet weak var date: UILabel!
+}
+//
+class YearMonth{
+    var year:Int
+    var month:Int
+    init() {
+        year = 2019
+        month = 2
+    }
+    init(Year:Int,Month:Int) {
+        self.month = Month
+        self.year = Year
+    }
+    var date:Date{
+        get{
+            let calendar = Calendar.current
+            var dateComponent = DateComponents()
+            dateComponent.month = month
+            dateComponent.year = year
+            return calendar.date(from: dateComponent)!
+        }
+    }
+    func toDate()->Date{
+        let calendar = Calendar.current
+        var dateComponent = DateComponents()
+        dateComponent.month = month
+        dateComponent.year = year
+        return calendar.date(from: dateComponent)!
+    }
+    func toDate(Year:Int,Month:Int)->Date{
+        let calendar = Calendar.current
+        var dateComponent = DateComponents()
+        dateComponent.month = Month
+        dateComponent.year = Year
+        return calendar.date(from: dateComponent)!
+    }
+    func preMonth()->YearMonth{
+        let nY = month==1 ? year-1:year
+        let nM = month==1 ? 12:month-1
+        return YearMonth.init(Year: nY, Month: nM)
+    }
+    func nextMonth()->YearMonth{
+        let nY = month==12 ? year+1:year
+        let nM = month==12 ? 1:month+1
+        return YearMonth.init(Year: nY, Month: nM)
+    }
 }
